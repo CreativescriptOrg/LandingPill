91b408972ef8a8d3bb84b86bee72d4457d526e7b
Yash Shivhare <yashshivhare@Yashs-Mac-Studio.local>
2024-08-28 20:25:25 +0530

    project image updated

--- src/components/LandingPage/Sections/Projects/Projects.tsx
+++ src/components/LandingPage/Sections/Projects/Projects.tsx
@@ -9,75 +9,85 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { CarouselButtonNext, CarouselButtonPrev } from "@/assets/vectors";
 
+const Images = [
+  { before: "/before1.webp", after: "/after1.webp" },
+  { before: "/before2.webp", after: "/after2.webp" },
+  { before: "/before3.webp", after: "/after3.webp" },
+  { before: "/before4.webp", after: "/after4.webp" },
+  { before: "/before5.webp", after: "/after5.webp" },
+  { before: "/before6.webp", after: "/after6.webp" },
+  { before: "/before7.webp", after: "/after7.webp" },
+];
+
 const Projects = () => {
-	return (
-		<section id='works' className={styles.container}>
-			<SectionTitle
-				title='My Creative work that drives crazy 
-results for our clients.'
-			/>
-			<div className={styles.subcontainer}>
-				<div className={styles.swiperContainer}>
-					<button className='swiper-button-custom projects-button-prev'>
-						<CarouselButtonPrev />
-					</button>
-					<div className={styles.main}>
-						<Swiper
-							spaceBetween={30}
-							navigation={{
-								nextEl: ".projects-button-next",
-								prevEl: ".projects-button-prev",
-							}}
-							modules={[Navigation]}
-							slidesPerView={1}
-							className={styles.projects}
-						>
-							{Array.from({
-								length: 4,
-							}).map((el, i) => {
-								return (
-									<SwiperSlide>
-										<div className={styles.slideContent}>
-											<div className={styles.before}>
-												<span>Before</span>
-												<div className={styles.imageBg}>
-													<Image
-														src={"/beforeProject.png"}
-														alt=''
-														width={500}
-														height={276}
-													/>
-												</div>
-											</div>
-											<div className={styles.after}>
-												<span>After</span>
-												<div className={styles.imageBg}>
-													<Image
-														src={"/afterProject.png"}
-														alt=''
-														width={500}
-														height={276}
-													/>
-												</div>
-											</div>
-										</div>
-									</SwiperSlide>
-								);
-							})}
-						</Swiper>
-					</div>
-					<button className='swiper-button-custom projects-button-next'>
-						<CarouselButtonNext />
-					</button>
-				</div>
-				<CTA
-					title='Wanna redesign from scratch ?'
-					buttonText='Get it redesigned'
-					icon='info'
-				/>
-			</div>
-		</section>
-	);
+  return (
+    <section id="works" className={styles.container}>
+      <SectionTitle
+        title="My Creative work that drives crazy 
+results for our clients."
+      />
+      <div className={styles.subcontainer}>
+        <div className={styles.swiperContainer}>
+          <button className="swiper-button-custom projects-button-prev">
+            <CarouselButtonPrev />
+          </button>
+          <div className={styles.main}>
+            <Swiper
+              spaceBetween={30}
+              navigation={{
+                nextEl: ".projects-button-next",
+                prevEl: ".projects-button-prev",
+              }}
+              modules={[Navigation]}
+              slidesPerView={1}
+              className={styles.projects}
+            >
+              {Array.from({
+                length: 7,
+              }).map((el, i) => {
+                return (
+                  <SwiperSlide>
+                    <div className={styles.slideContent}>
+                      <div className={styles.before}>
+                        <span>Before</span>
+                        <div className={styles.imageBg}>
+                          <Image
+                            src={Images[i].before}
+                            alt=""
+                            width={500}
+                            height={276}
+                          />
+                        </div>
+                      </div>
+                      <div className={styles.after}>
+                        <span>After</span>
+                        <div className={styles.imageBg}>
+                          <Image
+                            src={Images[i].after}
+                            alt=""
+                            width={500}
+                            height={276}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+          <button className="swiper-button-custom projects-button-next">
+            <CarouselButtonNext />
+          </button>
+        </div>
+        <CTA
+          title="Wanna redesign from scratch ?"
+          buttonText="Get it redesigned"
+          icon="info"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
