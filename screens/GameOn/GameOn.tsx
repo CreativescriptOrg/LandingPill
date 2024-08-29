5616b49c335972148d1fed1dbbecbada39af2854
Sarthak <sarthak2019fractal@gmail.com>
2024-08-29 23:24:11 +0530

    changes

--- screens/GameOn/GameOn.tsx
+++ screens/GameOn/GameOn.tsx
@@ -8,97 +8,97 @@ import { useState } from "react";
 import { CircularProgress } from "@mui/material";
 
 const GameOn = ({
-	type,
-	formState,
-	setFormData,
-	setStep,
+  type,
+  formState,
+  setFormData,
+  setStep,
 }: {
-	type: string;
-	formState: any;
-	setFormData: any;
-	setStep: any;
+  type: string;
+  formState: any;
+  setFormData: any;
+  setStep: any;
 }) => {
-	const router = useRouter();
-	const [loading, setLoading] = useState(false);
-	const handleSubmit = async (e: any) => {
-		e.preventDefault();
-		setLoading(false);
-		const res = await submitForm(formState);
-		if (res.status === "success") {
-			setLoading(false);
-			setStep();
-		}
-	};
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(false);
+    const res = await submitForm(formState);
+    if (res.status === "success") {
+      setLoading(false);
+      setStep();
+    }
+  };
 
-	return (
-		<main className='main_container center_content'>
-			<div className={styles.container}>
-				<div className={`title_content`}>
-					<h1 className='heading_1_sb'>{STRINGS[type].title}</h1>
-					<span className='subtitle_1_sb'>{STRINGS[type].subtitle}</span>
-				</div>
-				<div className={styles.flex}>
-					<div>
-						<Image src={"/founder1.png"} alt='' width={24} height={24} />
-						<Image src={"/founder2.png"} alt='' width={24} height={24} />
-						<Image src={"/founder3.png"} alt='' width={24} height={24} />
-					</div>
-					<span className='subtitle_1_re'>Approved by 17+ Founders</span>
-				</div>
+  return (
+    <main className="main_container center_content">
+      <div className={styles.container}>
+        <div className={`title_content`}>
+          <h1 className="heading_1_sb">{STRINGS[type].title}</h1>
+          <span className="subtitle_1_sb">{STRINGS[type].subtitle}</span>
+        </div>
+        <div className={styles.flex}>
+          <div>
+            <Image src={"/founder1.png"} alt="" width={24} height={24} />
+            <Image src={"/founder2.png"} alt="" width={24} height={24} />
+            <Image src={"/founder3.png"} alt="" width={24} height={24} />
+          </div>
+          <span className="subtitle_1_re">Approved by 17+ Founders</span>
+        </div>
 
-				<Cards type={type} />
+        <Cards type={type} />
 
-				<div className={styles.text2}>
-					Don’t wait! Pop in your email for instant access to your guide!
-				</div>
+        <div className={styles.text2}>
+          Don’t wait! Pop in your email for instant access to your guide!
+        </div>
 
-				<form
-					className={styles.cards_container}
-					onSubmit={(e) => handleSubmit(e)}
-				>
-					<Input
-						type='email'
-						placeholder='Need your email'
-						label='email'
-						name='email'
-						id='email'
-						required
-						hideLabel={true}
-						onChange={(e) => setFormData(e.target.value)}
-						error=''
-						value={formState.email}
-					/>
-					<div className={`submit_container`}>
-						<button className='button_primary' disabled={!formState.email}>
-							Access now{" "}
-							{loading && (
-								<CircularProgress sx={{ color: "white" }} size={20} />
-							)}
-						</button>
-					</div>
-				</form>
-			</div>
-		</main>
-	);
+        <form
+          className={styles.cards_container}
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <Input
+            type="email"
+            placeholder="Need your email"
+            label="email"
+            name="email"
+            id="email"
+            required
+            hideLabel={true}
+            onChange={(e) => setFormData(e.target.value)}
+            error=""
+            value={formState.email}
+          />
+          <div className={`submit_container`}>
+            <button className="button_primary" disabled={!formState.email}>
+              Access now{" "}
+              {loading && (
+                <CircularProgress sx={{ color: "white" }} size={20} />
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default GameOn;
 export const Cards = ({ type }: { type: any }) => {
-	return (
-		<div className={styles.flex1}>
-			{STRINGS[type].data.map((item: any) => {
-				return (
-					<div className={styles.box} key={item?.title}>
-						<div className={styles.flex2}>
-							<Image src={item?.img} alt='img' height={28} width={28} />
-							<div>
-								<div className={styles.text}>{item?.title}</div>
-								<div className={styles.text1}>{item?.des}</div>
-							</div>
-						</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className={styles.flex1}>
+      {STRINGS[type].data.map((item: any) => {
+        return (
+          <div className={styles.box} key={item.text}>
+            <div className={styles.flex2}>
+              <Image src={item?.img} alt="img" height={28} width={28} />
+              <div>
+                <div className={styles.text}>{item?.title}</div>
+                <div className={styles.text1}>{item?.des}</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -5,56 +5,54 @@ import styles from "./Tooltip.module.css";
 import Image from "next/image";
 
 const Tooltip = ({ setShowImages }: { setShowImages: any }) => {
-	return (
-		<div className={styles.container}>
-			<Typewriter
-				options={{
-					delay: 50, // Speed for typing (lower value means faster typing)
-					deleteSpeed: 0.00001, // Speed for deleting (lower value means faster deleting)
-				}}
-				onInit={(typewriter) => {
-					typewriter
-						.typeString(
-							'<span class="typewriter_text subtitle_1_sb">Hey <br/> Founders!</span>'
-						)
-						// .pauseFor(300)
-						.deleteChars(20)
-						.typeString(
-							'<span class="typewriter_text subtitle_1_sb"><strong>I’m Landing Pill, </strong><br/> Your Landing Page Doc, I’m here to help you in fixing your landing page.</span>'
-						)
-						// .pauseFor(300)
-						.deleteChars(100)
-						.typeString(
-							'<span class="typewriter_text subtitle_1_sb">But first, I need to get to know you a little better</span>'
-						)
-						.callFunction(() => {
-							setShowImages(true);
-						})
-						// .pauseFor(300)
-						// .deleteChars(90)
-						// .typeString(
-						// 	'<span class="typewriter_text subtitle_1_sb"><strong>The results were quite good, have a look</strong><br/><strong><span style="margin-bottom: 6px;">. </span></strong>increased conversion up to 30%<br/><strong>. </strong>user footfall increased by 10%</span>'
-						// )
-						// .pauseFor(300)
-						// .deleteChars(110)
-						// .callFunction(() => {
-						// 	setShowImages(true);
-						// })
-						// .typeString(
-						// 	'<span class="typewriter_text subtitle_1_sb"><strong>Here are some of my work</strong>'
-						// )
-						.start();
-				}}
-			/>
-			<Image
-				src={Arrow}
-				width={12}
-				height={12}
-				alt='Arrow'
-				className={styles.arrow}
-			/>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <Typewriter
+        options={{
+          delay: 50, // Speed for typing (lower value means faster typing)
+          deleteSpeed: 0.00001, // Speed for deleting (lower value means faster deleting)
+        }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString(
+              '<span id="first-string" class="typewriter_text subtitle_1_sb">Hey <br/> Founders!</span>'
+            )
+            .pauseFor(1000) // Pause before hiding the text
+            .callFunction(() => {
+              const element = document.getElementById("first-string");
+              if (element) {
+                element.style.display = "none";
+              }
+            })
+            .typeString(
+              '<span id="second-string" class="typewriter_text subtitle_1_sb"><strong>I’m Landing Pill, </strong><br/> Your Landing Page Doc, I’m here to help you in fixing your landing page.</span>'
+            )
+            .pauseFor(1000) // Pause before hiding the text
+            .callFunction(() => {
+              const element = document.getElementById("second-string");
+              if (element) {
+                element.style.display = "none";
+              }
+            })
+            .typeString(
+              '<span id="third-string" class="typewriter_text subtitle_1_sb">But first, I need to get to know you a little better</span>'
+            )
+            .callFunction(() => {
+              setShowImages(true);
+            })
+            .start();
+        }}
+      />
+
+      <Image
+        src={Arrow}
+        width={12}
+        height={12}
+        alt="Arrow"
+        className={styles.arrow}
+      />
+    </div>
+  );
 };
 
 export default Tooltip;
