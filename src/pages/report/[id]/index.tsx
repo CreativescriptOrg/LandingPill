4e9df04cfe0ad4256578db7a581abb3116651286
Sarthak <sarthak2019fractal@gmail.com>
2025-03-22 23:45:25 +0530

    fixes

--- src/pages/report/[id]/index.tsx
+++ src/pages/report/[id]/index.tsx
@@ -12,143 +12,144 @@ import { ResponsiveImage } from "@/components/report/shared/ResponsiveImage";
 import { Loading } from "@/components/report/shared/Loading";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { ALL_LINKS } from "../../../../config";
 
 interface ReportData {
-	report_status: string;
-	payment_status: string;
-	name: string;
-	date: string;
-	emailId: string;
-	url: string;
-	sectionAudit?: any;
-	finalReport?: {
-		LandingPageReport?: {
-			ExecutiveSummary?: any;
-			KeyTakeaways?: any;
-		};
-	};
-	seoAudit?: any;
+  report_status: string;
+  payment_status: string;
+  name: string;
+  date: string;
+  emailId: string;
+  url: string;
+  sectionAudit?: any;
+  finalReport?: {
+    LandingPageReport?: {
+      ExecutiveSummary?: any;
+      KeyTakeaways?: any;
+    };
+  };
+  seoAudit?: any;
 }
 
 const Report = () => {
-	const router = useRouter();
-	const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
 
-	const [reportData, setReportData] = useState<ReportData | null>(null);
-	const [loading, setLoading] = useState(true);
-	const [generatingReport, setGeneratingReport] = useState(false);
-	const [isPaymentDone, setIsPaymentDone] = useState(false);
-	const [date, setDate] = useState("");
+  const [reportData, setReportData] = useState<ReportData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [generatingReport, setGeneratingReport] = useState(false);
+  const [isPaymentDone, setIsPaymentDone] = useState(false);
+  const [date, setDate] = useState("");
 
-	const fetchReportData = async () => {
-		try {
-			const res = await axios.get(
-				`https://pill.estulife.com/api/v1/page/getPaymentStatus?orderId=${id}`
-			);
-			let report = res.data.data;
-			setReportData(report);
-			setGeneratingReport(
-				report?.report_status === "pending" &&
-					report?.payment_status !== "pending"
-			);
-			setIsPaymentDone(report?.payment_status !== "pending");
+  const fetchReportData = async () => {
+    try {
+      const res = await axios.get(
+        `${ALL_LINKS.BASE_URL}/api/v1/page/getPaymentStatus?orderId=${id}`
+      );
+      let report = res.data.data;
+      setReportData(report);
+      setGeneratingReport(
+        report?.report_status === "pending" &&
+          report?.payment_status !== "pending"
+      );
+      setIsPaymentDone(report?.payment_status !== "pending");
 
-			const date = new Date(report?.date || "");
-			const formattedDate = `${date.getDate()} ${date.toLocaleString(
-				"default",
-				{
-					month: "short",
-				}
-			)}, ${date.getFullYear()}`;
+      const date = new Date(report?.date || "");
+      const formattedDate = `${date.getDate()} ${date.toLocaleString(
+        "default",
+        {
+          month: "short",
+        }
+      )}, ${date.getFullYear()}`;
 
-			setDate(formattedDate);
-			setLoading(false);
-		} catch (error) {
-			console.error("Error fetching report data:", error);
-			setLoading(false);
-		}
-	};
+      setDate(formattedDate);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching report data:", error);
+      setLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		if (id) {
-			fetchReportData();
-		}
-	}, [id]);
+  useEffect(() => {
+    if (id) {
+      fetchReportData();
+    }
+  }, [id]);
 
-	useEffect(() => {
-		if (generatingReport) {
-			const interval = setInterval(async () => {
-				fetchReportData();
-			}, 3000);
+  useEffect(() => {
+    if (generatingReport) {
+      const interval = setInterval(async () => {
+        fetchReportData();
+      }, 3000);
 
-			return () => clearInterval(interval);
-		}
-	}, [reportData, id, generatingReport]);
+      return () => clearInterval(interval);
+    }
+  }, [reportData, id, generatingReport]);
 
-	if (loading || generatingReport) {
-		return <Loading />;
-	}
+  if (loading || generatingReport) {
+    return <Loading />;
+  }
 
-	return (
-		<>
-			<div className='min-h-screen bg-gray-50'>
-				{reportData && <Header name={reportData.name} date={date} />}
+  return (
+    <>
+      <div className="min-h-screen bg-gray-50">
+        {reportData && <Header name={reportData.name} date={date} />}
 
-				<main className='max-w-5xl mx-auto px-4 sm:px-6 py-8 sm:pt-32 sm:pb-12'>
-					{!isPaymentDone ? (
-						<>
-							{reportData?.sectionAudit && (
-								<DetailedAnalysis
-									data={[reportData?.sectionAudit]}
-									isPaymentDone={false}
-									url={reportData?.url}
-								/>
-							)}
-							<div className='razorpay_container'>
-								<div className='razorpay_backdrop'></div>
-								<ResponsiveImage
-									src={"/razorpay_bg.png"}
-									alt='Hero Section Screenshot'
-								/>
-								<div className='razorpay_btn'>
-									<RazorpayCheckout orderId={id} email={reportData?.emailId} />
-								</div>
-							</div>
-						</>
-					) : (
-						<>
-							{reportData?.finalReport?.LandingPageReport?.ExecutiveSummary && (
-								<ExecutiveSummary
-									data={
-										reportData.finalReport.LandingPageReport.ExecutiveSummary
-									}
-								/>
-							)}
-							{reportData?.finalReport?.LandingPageReport?.KeyTakeaways && (
-								<KeyTakeaways
-									data={reportData.finalReport.LandingPageReport.KeyTakeaways}
-								/>
-							)}
-							{reportData?.sectionAudit && (
-								<DetailedAnalysis
-									data={reportData?.sectionAudit}
-									isPaymentDone
-								/>
-							)}
+        <main className="max-w-5xl mx-auto px-4 sm:px-6 py-8 sm:pt-32 sm:pb-12">
+          {!isPaymentDone ? (
+            <>
+              {reportData?.sectionAudit && (
+                <DetailedAnalysis
+                  data={[reportData?.sectionAudit]}
+                  isPaymentDone={false}
+                  url={reportData?.url}
+                />
+              )}
+              <div className="razorpay_container">
+                <div className="razorpay_backdrop"></div>
+                <ResponsiveImage
+                  src={"/razorpay_bg.png"}
+                  alt="Hero Section Screenshot"
+                />
+                <div className="razorpay_btn">
+                  <RazorpayCheckout orderId={id} email={reportData?.emailId} />
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              {reportData?.finalReport?.LandingPageReport?.ExecutiveSummary && (
+                <ExecutiveSummary
+                  data={
+                    reportData.finalReport.LandingPageReport.ExecutiveSummary
+                  }
+                />
+              )}
+              {reportData?.finalReport?.LandingPageReport?.KeyTakeaways && (
+                <KeyTakeaways
+                  data={reportData.finalReport.LandingPageReport.KeyTakeaways}
+                />
+              )}
+              {reportData?.sectionAudit && (
+                <DetailedAnalysis
+                  data={reportData?.sectionAudit}
+                  isPaymentDone
+                />
+              )}
 
-							{/* <PerformanceSEO data={reportData.PerformanceSEO} /> */}
-							{reportData?.seoAudit && (
-								<SEOAuditFindings data={reportData.seoAudit} />
-							)}
-							{/* <FinalActionPlan data={reportData.FinalActionPlan} /> */}
-						</>
-					)}
-				</main>
+              {/* <PerformanceSEO data={reportData.PerformanceSEO} /> */}
+              {reportData?.seoAudit && (
+                <SEOAuditFindings data={reportData.seoAudit} />
+              )}
+              {/* <FinalActionPlan data={reportData.FinalActionPlan} /> */}
+            </>
+          )}
+        </main>
 
-				<Footer />
-			</div>
-		</>
-	);
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default Report;
--- src/utils/submitForm.ts
+++ src/utils/submitForm.ts
@@ -1,99 +1,100 @@
 import axios from "axios";
+import { ALL_LINKS } from "../../config";
 
 const submitForm = async (formState: any) => {
-	let data = JSON.stringify(formState);
-	try {
-		const res = await axios.post(
-			"https://pill.estulife.com/api/v1/page/storepagedata",
-			data,
-			{
-				maxBodyLength: Infinity,
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		);
-		if (res?.data) {
-			return res.data
-				? res.data
-				: {
-						status: "error",
-						message: "Something went wrong",
-				  };
-		}
-	} catch (error) {
-		let message = (error as any).response?.data?.message;
-		return {
-			status: "failed",
-			message: message ? message : "Something went wrong",
-		};
-	}
+  let data = JSON.stringify(formState);
+  try {
+    const res = await axios.post(
+      `${ALL_LINKS.BASE_URL}/api/v1/page/storepagedata`,
+      data,
+      {
+        maxBodyLength: Infinity,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res?.data) {
+      return res.data
+        ? res.data
+        : {
+            status: "error",
+            message: "Something went wrong",
+          };
+    }
+  } catch (error) {
+    let message = (error as any).response?.data?.message;
+    return {
+      status: "failed",
+      message: message ? message : "Something went wrong",
+    };
+  }
 };
 
 export const sendEmail = async (formState: any) => {
-	let data = JSON.stringify({
-		to: formState.email,
-		subject: "Got It! Your Landing Page Is in the Lab 💊",
-		message:
-			"Hey,\n\nWe've received your landing page details and the Landing Pill team is already on it! 🛠️\n\nWe'll dive deep, analyze what's working (and what's not), and get back to you with actionable insights soon.\n\nSit tight—your landing page transformation is coming. 🚀\n\nTalk soon,\nTeam Landing Pill",
-	});
+  let data = JSON.stringify({
+    to: formState.email,
+    subject: "Got It! Your Landing Page Is in the Lab 💊",
+    message:
+      "Hey,\n\nWe've received your landing page details and the Landing Pill team is already on it! 🛠️\n\nWe'll dive deep, analyze what's working (and what's not), and get back to you with actionable insights soon.\n\nSit tight—your landing page transformation is coming. 🚀\n\nTalk soon,\nTeam Landing Pill",
+  });
 
-	try {
-		const res = await axios.post(
-			"https://zka84ijxrc.execute-api.us-east-1.amazonaws.com/dev/gmailSend",
-			data,
-			{
-				maxBodyLength: Infinity,
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		);
-		if (res?.data) {
-			return res.data
-				? res.data
-				: {
-						status: "error",
-						message: "Something went wrong",
-				  };
-		}
-	} catch (error) {
-		let message = (error as any).response?.data?.message;
-		return {
-			status: "failed",
-			message: message ? message : "Something went wrong",
-		};
-	}
+  try {
+    const res = await axios.post(
+      "https://zka84ijxrc.execute-api.us-east-1.amazonaws.com/dev/gmailSend",
+      data,
+      {
+        maxBodyLength: Infinity,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res?.data) {
+      return res.data
+        ? res.data
+        : {
+            status: "error",
+            message: "Something went wrong",
+          };
+    }
+  } catch (error) {
+    let message = (error as any).response?.data?.message;
+    return {
+      status: "failed",
+      message: message ? message : "Something went wrong",
+    };
+  }
 };
 
 export const getHeroSectionAuditForm = async (formState: any) => {
-	let data = JSON.stringify(formState);
-	try {
-		const res = await axios.post(
-			"https://pill.estulife.com/api/v1/page/getHeroSectionAudit",
-			data,
-			{
-				maxBodyLength: Infinity,
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		);
-		if (res?.data) {
-			return res.data
-				? res.data
-				: {
-						status: "error",
-						message: "Something went wrong",
-				  };
-		}
-	} catch (error) {
-		let message = (error as any).response?.data?.message;
-		return {
-			status: "failed",
-			message: message ? message : "Something went wrong",
-		};
-	}
+  let data = JSON.stringify(formState);
+  try {
+    const res = await axios.post(
+      `${ALL_LINKS.BASE_URL}/api/v1/page/getHeroSectionAudit`,
+      data,
+      {
+        maxBodyLength: Infinity,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res?.data) {
+      return res.data
+        ? res.data
+        : {
+            status: "error",
+            message: "Something went wrong",
+          };
+    }
+  } catch (error) {
+    let message = (error as any).response?.data?.message;
+    return {
+      status: "failed",
+      message: message ? message : "Something went wrong",
+    };
+  }
 };
 
 export default submitForm;
