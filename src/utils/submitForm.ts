584d6c6d16dc8e35d6aba03f8441711a9c38383b
Sarthak <sarthak2019fractal@gmail.com>
2025-04-04 23:57:03 +0530

    changes

--- src/utils/submitForm.ts
+++ src/utils/submitForm.ts
@@ -66,9 +66,33 @@ export const sendEmail = async (formState: any) => {
     };
   }
 };
+function formatLink(userInput: any) {
+  // Remove any leading/trailing whitespace
+  let link = userInput.trim();
 
+  // Check if the link starts with http:// or https:// and remove it
+  if (link.startsWith("http://")) {
+    link = link.substring(7);
+  } else if (link.startsWith("https://")) {
+    link = link.substring(8);
+  }
+
+  // Check if the link starts with www. and add it if not
+  if (!link.startsWith("www.")) {
+    link = "www." + link;
+  }
+
+  // Add https:// prefix
+  link = "https://" + link;
+
+  return link;
+}
 export const getHeroSectionAuditForm = async (formState: any) => {
-  let data = JSON.stringify(formState);
+  let data: any = JSON.stringify({
+    ...formState,
+    url: formatLink(formState?.url),
+  });
+
   try {
     const res = await axios.post(
       `${ALL_LINKS.BASE_URL}/api/v1/page/getHeroSectionAudit`,
